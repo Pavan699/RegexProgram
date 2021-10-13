43623d4e79457a41e561bd368a7e046c00282e5a
Pavan699 <pavannakate03@gmail.com>
2021-10-13 00:13:52 -0700

    Adding Reflections to User Registrations

--- RegexProgram/Program.cs
+++ RegexProgram/Program.cs
@@ -52,6 +52,7 @@ namespace RegexProgram
                 }
             }
             Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            Reflections.ReflectionsTests();
         }
     }
 }
---
+++ RegexProgram/Reflections.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace RegexProgram
+{
+    class Reflections
+    {
+        /// <summary>
+        /// RflectionTest method to get the matadata for the methods and constructors
+        /// </summary>
+        public static void ReflectionsTests()
+        {
+            Type type = typeof(Inputs);
+            Console.WriteLine("Full Name : {0}",type.FullName);
+            Console.WriteLine("Class Name : {0}",type.Name);
+
+            MethodInfo[] methods = type.GetMethods();
+            foreach(MethodInfo method in methods)
+            {
+                Console.WriteLine(method.ReturnType.Name +"  " + method.Name);
+            }
+
+            ConstructorInfo[] constructors = type.GetConstructors();
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine(constructor.ToString());
+            }
+        }
+    }
+}
